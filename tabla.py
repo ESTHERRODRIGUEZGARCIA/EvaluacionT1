fa3ee71c6096f57162d6d9ff1655e5075b65165f
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-09-23 21:14:47 +0200

    lo tenía mal colocado

--- tabla.py
+++ tabla.py
@@ -5,18 +5,11 @@ def tabla():
     fila = int(input("Introduzca un argumento "))
     columna = int(input("Introduzca un argumento "))
 
-    if fila in range (0,9):
-        print()
-    else:
+    if fila < 1 or fila > 9 or columna < 1 or columna > 9:
         print("Error.")
 
-    if columna in range (0,9):
-        print()
     else:
-        print("Error.")
-
-
-    for i in range(fila):
+        for i in range(fila):
             for i in range(columna):
                 print(" * ", end='')
 
