e68b246320f1fb39b870ed5b7ef03589099b5610
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-09-23 21:35:44 +0200

    prueba

--- ejer1.py
+++ ejer1.py
@@ -1,4 +1,6 @@
-matriz = [
+
+def sum(matriz):
+    matriz = [
     [1, 1, 1, 3],
 
     [2, 2, 2, 7],
@@ -6,9 +8,8 @@ matriz = [
     [3, 3, 3, 9],
 
     [4, 4, 4, 13]
-]
+    ]
 
-def sum(matriz):
     for i in matriz():
         matriz[1][3] = sum(matriz[1][:-1])
         matriz[3][3] = sum( matriz[3][:-1])
