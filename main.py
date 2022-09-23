483bbc42c2a2ee0bd2b13eccac2f2d17b69035f5
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-09-23 16:26:41 +0200

    save

--- main.py
+++ main.py
@@ -5,9 +5,17 @@ from ejercicios import *
 def ejercicio():
     variable = int(input("Seleccione que ejercicio desea ejecutar: \n1. \n2. \n3. \n4. \n"))
     if variable == 1:
-        
-        print(ejer1)
 
-        
+        print(ejer1)
+    if variable == 2:
+
+        print(ejer2)
+    if variable == 3:
+
+        print(ejer3)
+    if variable == 4:
+
+        print(ejer4)
+
 
 ejercicio()
