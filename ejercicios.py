9abf64f11a96b51658997e80fe75d6f7fd032a19
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-09-23 21:40:17 +0200

    a ver ahora

--- ejercicios.py
+++ ejercicios.py
@@ -17,7 +17,6 @@ class Ejer1():
         matriz()
 
 class Ejer2():
-        
     #dirá si tiene una longitud mayor o igual que 3 y a su vez es menor que 10
 
     def cadena_texto():
@@ -27,3 +26,30 @@ class Ejer2():
             print("Su texto es mayor o igual que 3 y a su vez es menor que 10 \nTrue.")
         else:
             print("La longitud de su texto no es mayor o igual que 3 ni menor que 10\nFalse.")
+
+
+class Ejer3():
+    def numeros():
+        print(list(range(0,11)))
+        print(list(range(-10,0)))
+        print(list(range(0, 21, 2)))
+        print(list(range(-19, 0, 2)))
+        print(list(range(0, 51, 5)))
+
+class tabla():
+    
+
+    def tabla():
+
+        fila = int(input("Introduzca un número de filas "))
+        columna = int(input("Introduzca un número de columnas "))
+
+        if fila < 1 or fila > 9 or columna < 1 or columna > 9:
+            print("Error.")
+
+        else:
+            for i in range(fila):
+                print(" ")
+                for j in range(columna):
+                    print(" * ", end='')
+
