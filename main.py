a698c2c1908a9a62b34e579d32df44889e72f906
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-09-23 16:45:37 +0200

    error modificado

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
 
-from ejer1 import *
-from ejer2 import *
-from ejer3 import *
-from ejer4 import *
-
-def ejercicio():
-    variable = int(input("Seleccione que ejercicio desea ejecutar: \n1. \n2. \n3. \n4. \n"))
-    if variable == 1:
-        print(ejer1)
-
-    if variable == 2:
-
-        print(ejer2)
-    if variable == 3:
-
-        print(ejer3)
-    if variable == 4:
-
-        print(ejer4)
+import ejer1
+import ejer2
+import ejer3
+import tabla
 
 
-ejercicio()
+variable = int(input("Seleccione que ejercicio desea ejecutar: \n1. \n2. \n3. \n4. \n"))
+if variable == 1:
+    print(ejer1)
+if variable == 2:
+    print(ejer2)
+if variable == 3:
+    print(ejer3)
+if variable == 4:
+    print(tabla)
+
