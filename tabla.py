42dfc834873d5a7d42b6a2a758558f3a042d76ba
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-09-23 20:57:06 +0200

    entre 0,9

--- tabla.py
+++ tabla.py
@@ -1,4 +1,13 @@
 def tabla():
     fila = input("Introduzca un argumento ")
     columna = input("Introduzca un argumento ")
+    if fila in range (0,9):
+        print()
+    else:
+        print("Error.")
+
+    if columna in range (0,9):
+        print()
+    else:
+        print("Error.")
     
