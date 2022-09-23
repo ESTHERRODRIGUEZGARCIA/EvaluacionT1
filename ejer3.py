270d214692e05cb779ffe8f120df4e03f0e6ea9d
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-09-23 16:42:11 +0200

    3 fin

--- ejer3.py
+++ ejer3.py
@@ -1,5 +1,5 @@
 print(list(range(0,11)))
 print(list(range(-10,0)))
 print(list(range(0, 21, 2)))
-print(list(range(-19,0, 2)))
-print(list(range(0, 50, 5)))
+print(list(range(-19, 0, 2)))
+print(list(range(0, 51, 5)))
