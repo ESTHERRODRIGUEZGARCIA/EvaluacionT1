e2a52141203105fd5cb31c00db1a363c9d1cabeb
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-09-23 16:25:50 +0200

    main

---
+++ ejercicios/ejer1.py
@@ -0,0 +1,17 @@
+matriz = [
+    [1, 1, 1, 3],
+
+    [2, 2, 2, 7],
+
+    [3, 3, 3, 9],
+
+    [4, 4, 4, 13]
+]
+
+def sum(matriz):
+    for i in matriz():
+        matriz[1][3] = sum(matriz[1][:-1])
+        matriz[3][3] = sum( matriz[3][:-1])
+
+        matriz()
+
