339d65e3dae9903c7c90f063a6168401e98c8a41
ESTHERRODRIGUEZGARCIA <91721860+ESTHERRODRIGUEZGARCIA@users.noreply.github.com>
2022-09-23 16:09:26 +0200

    guardar main

---
+++ main.py
@@ -0,0 +1,11 @@
+
+from ejercicios import *
+
+
+def ejercicio():
+    variable = int(input("Seleccione que ejercicio desea ejecutar: \n1. \n2. \n3. \n4. \n"))
+    if variable == 1:
+        
+
+
+ejercicio()
